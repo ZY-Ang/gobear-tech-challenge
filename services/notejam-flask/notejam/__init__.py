07ba18985287db7cab69a559b6bff9039a607d68
Alex Ang <angzhanyu@gmail.com>
2020-03-09 07:22:48 +0800

    Update flask css

--- services/notejam-flask/notejam/__init__.py
+++ services/notejam-flask/notejam/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 from flask.ext.mail import Mail
@@ -17,7 +17,7 @@ from_env = {'production': ProductionConfig,
             'dbconfig': Config}
 
 # @TODO use application factory approach
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='/flask/static', static_folder='static')
 app.config.from_object(from_env[os.environ.get('ENVIRONMENT', 'testing')])
 if os.environ.get('ENVIRONMENT') == 'production':
     import sqlalchemy
