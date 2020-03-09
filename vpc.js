5f776ad60430d69477394730d576e40e07cd8e99
Alex Ang <angzhanyu@gmail.com>
2020-03-09 20:45:48 +0800

    final wip

--- vpc.js
+++ vpc.js
@@ -1,5 +1,5 @@
 /**
- * This file exports env vars from stack outputs for use with CI
+ * This file exports env vars from the default vpc for use with CI
  */
 
 const AWS = require("aws-sdk");
@@ -21,46 +21,47 @@ const getAWSAccountId = (credentials) => new Promise((resolve, reject) =>
         })
 );
 
-const getCloudFormationExport = async (credentials, exportName) => {
-    const exports = await new Promise((resolve, reject) =>
-        (new AWS.CloudFormation({credentials}))
-            .listExports({}, (err, data) => {
+const getDefaultVpcId = async (credentials) => {
+    const {Vpcs} = await new Promise((resolve, reject) =>
+        (new AWS.EC2({credentials}))
+            .describeVpcs({Filters: [{Name: "isDefault", Values: ["true"]}]}, (err, data) => {
                 if (err) {
-                    console.error("Error while listing cloudformation exports.");
+                    console.error("Error while calling ec2.describeVpcs. You most likely forgot to set up aws credentials. See https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-shared.html for more information");
                     reject(err);
+                } else if (!data.Vpcs || !data.Vpcs.length) {
+                    console.error("Error while getting data.Vpcs. This is unexpected.");
+                    reject(data);
                 } else {
                     resolve(data);
                 }
             })
     );
-    if (exports.Exports) {
-        for (let exp of exports.Exports) {
-            if (exp.Name === exportName) {
-                return exp.Value;
-            }
-        }
-    }
-    throw new Error(`Export ${exportName} not found in exports: \n${JSON.stringify(exports)}`);
+
+    return Vpcs[0].VpcId;
 };
 
-const getCloudFormationStackOutput = async (credentials, StackName, outputName) => {
-    const {Stacks} = await new Promise((resolve, reject) =>
-        (new AWS.CloudFormation({credentials}))
-            .describeStacks({StackName}, (err, data) => {
+const getDefaultVpcSubnetIds = async (credentials, vpcId) => {
+    const {Subnets} = await new Promise((resolve, reject) =>
+        (new AWS.EC2({credentials}))
+            .describeSubnets({Filters: [{Name: "vpc-id", Values: [vpcId]}]}, (err, data) => {
                 if (err) {
+                    console.error("Error while calling ec2.describeSubnets. You most likely forgot to set up aws credentials. See https://docs.aws.amazon.com/sdk-for-javascript/v2/developer-guide/loading-node-credentials-shared.html for more information");
                     reject(err);
+                } else if (!data.Subnets || !data.Subnets.length) {
+                    console.error("Error while getting data.Vpcs. This is unexpected.");
+                    reject(data);
                 } else {
                     resolve(data);
                 }
             })
     );
 
-    return Stacks[0].Outputs.filter(({OutputKey}) => (OutputKey === outputName))[0].OutputValue;
+    return Subnets.map(({SubnetId}) => SubnetId);
 };
 
 const run = async () => {
-    if (process.argv.length !== 4) {
-        throw new Error("Invalid number of arguments. Please run node env.js <STACK_NAME> <STACK_OUTPUT>");
+    if (process.argv.length !== 3) {
+        throw new Error("Invalid number of arguments. Please run node env.js <vpc/subnet>");
     }
 
     // console.log("================== ENVIRONMENT VARIABLES ==================");
@@ -96,9 +97,16 @@ const run = async () => {
     // console.log(`AWS_REGION:\t\t\t\t\t\t${shell.env.AWS_REGION}${!shell.env.AWS_REGION?` => ${DEFAULT_AWS_REGION}`:''}`);
     shell.env.AWS_REGION = process.env.AWS_REGION || DEFAULT_AWS_REGION;
 
-    // ECRRepositoryArn
-    const OUTPUT = await getCloudFormationStackOutput(credentials, process.argv[2], process.argv[3]);
-    console.log(OUTPUT);
+    // Command
+    const defaultVpcId = await getDefaultVpcId(credentials);
+    if (process.argv[2].toLowerCase().includes('vpc')) {
+        console.log(defaultVpcId);
+    } else if (process.argv[2].toLowerCase().includes('subnet')) {
+        const vpcs = await getDefaultVpcSubnetIds(credentials, defaultVpcId);
+        console.log(vpcs.join(','));
+    } else {
+        throw new Error(`Unrecognized command ${process.argv[2]}. Please use one of vpc/subnet`);
+    }
 };
 
 module.exports = run;
